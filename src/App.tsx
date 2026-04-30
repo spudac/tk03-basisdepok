13889e6108066d340ffe4545b271be966b52f82c
spudac <iamaspuduck@gmail.com>
2026-04-30 15:49:11 +0700

    adding kelolaHadiah page and corresponding CRUD functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './features/auth/Auth.tsx';
 import Register from './features/register/Register.tsx';
+import KelolaHadiah from './features/ManageGifts/ManageGifts.tsx';
 // import Main from './pages/Main'; // The main page you navigate to after login
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         
         <Route path="/register" element={<Register />}></Route>
+
+        <Route path="/kelola-hadiah" element={<KelolaHadiah/>}></Route>
         
         {/* Redirect unknown routes to login */}
         <Route path="*" element={<Navigate to="/login" replace />} />
